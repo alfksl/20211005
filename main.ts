0ae8f3c50fc1086e2cbdca5150d6479449d0a060
alfksl <90828764+alfksl@users.noreply.github.com>
2021-10-07 11:39:36 +0900

    가변저항2

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+let 가변저항 = 0
 basic.forever(function () {
-	
+    가변저항 = pins.analogReadPin(AnalogPin.P0)
+    if (가변저항 > 0 && 가변저항 <= 350) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    } else if (350 < 가변저항 && 가변저항 <= 700) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    } else if (700 < 가변저항 && 가변저항 <= 1023) {
+        pins.digitalWritePin(DigitalPin.P5, 1)
+        pins.digitalWritePin(DigitalPin.P5, 0)
+    }
+    basic.showNumber(가변저항)
 })
