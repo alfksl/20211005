5186a453624a284489cd898a8b6b4ee6e373e368
alfksl <90828764+alfksl@users.noreply.github.com>
2021-10-07 11:53:50 +0900

    가변저항 오징어

--- main.ts
+++ main.ts
@@ -4,12 +4,15 @@ basic.forever(function () {
     if (가변저항 > 0 && 가변저항 <= 350) {
         pins.digitalWritePin(DigitalPin.P1, 1)
         pins.digitalWritePin(DigitalPin.P1, 0)
-    } else if (350 < 가변저항 && 가변저항 <= 700) {
-        pins.digitalWritePin(DigitalPin.P2, 1)
-        pins.digitalWritePin(DigitalPin.P2, 0)
-    } else if (700 < 가변저항 && 가변저항 <= 1023) {
-        pins.digitalWritePin(DigitalPin.P5, 1)
         pins.digitalWritePin(DigitalPin.P5, 0)
+    } else if (350 < 가변저항 && 가변저항 <= 700) {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.digitalWritePin(DigitalPin.P5, 0)
+    } else if (700 < 가변저항 && 가변저항 <= 1023) {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P5, 1)
     }
     basic.showNumber(가변저항)
 })
